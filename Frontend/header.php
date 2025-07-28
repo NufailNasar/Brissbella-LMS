c9f4ad821e96cc4447fa93e1ff352654aee796bd
NufailNasar <nufailnasar5367683@gmail.com>
2025-07-28 15:55:33 +0530

    Header problem solution Done
    Solved the header problems (Navigation issues)

--- Frontend/header.php
+++ Frontend/header.php
@@ -1,3 +1,7 @@
+<?php
+$currentPage = basename($_SERVER['PHP_SELF']);
+?>
+
 <head>
     <meta charset="utf-8">
     <title>Brissbella LMS</title>
@@ -38,30 +42,35 @@
     <!-- Spinner End -->
 
 
-    <!-- Navbar Start -->
-    <nav class="navbar navbar-expand-lg bg-white navbar-light shadow sticky-top p-0">
-        <a href="index.php" class="navbar-brand d-flex align-items-center px-4 px-lg-5">
-            <h2 class="m-0 text-primary"><i class="fa fa-book me-3"></i>Brissbella LMS</h2>
-        </a>
-        <button type="button" class="navbar-toggler me-4" data-bs-toggle="collapse" data-bs-target="#navbarCollapse">
-            <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="collapse navbar-collapse" id="navbarCollapse">
-            <div class="navbar-nav ms-auto p-4 p-lg-0">
-                <a href="index.php" class="nav-item nav-link active">Home</a>
-                <a href="about.php" class="nav-item nav-link">About</a>
-                <a href="courses.php" class="nav-item nav-link">Courses</a>
-                <div class="nav-item dropdown">
-                    <a href="#" class="nav-link dropdown-toggle" data-bs-toggle="dropdown">Pages</a>
-                    <div class="dropdown-menu fade-down m-0">
-                        <a href="team.php" class="dropdown-item">Our Team</a>
-                        <a href="testimonial.php" class="dropdown-item">Testimonial</a>
-                    </div>
+<!-- Navbar Start -->
+<nav class="navbar navbar-expand-lg bg-white navbar-light shadow sticky-top p-0">
+    <a href="index.php" class="navbar-brand d-flex align-items-center px-4 px-lg-5">
+        <h2 class="m-0 text-primary"><i class="fa fa-book me-3"></i>Brissbella LMS</h2>
+    </a>
+    <button type="button" class="navbar-toggler me-4" data-bs-toggle="collapse" data-bs-target="#navbarCollapse">
+        <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarCollapse">
+        <div class="navbar-nav ms-auto p-4 p-lg-0">
+            <a href="index.php" class="nav-item nav-link <?= $currentPage == 'index.php' ? 'active' : '' ?>">Home</a>
+            <a href="about.php" class="nav-item nav-link <?= $currentPage == 'about.php' ? 'active' : '' ?>">About</a>
+            <a href="courses.php" class="nav-item nav-link <?= $currentPage == 'courses.php' ? 'active' : '' ?>">Courses</a>
+
+            <div class="nav-item dropdown">
+                <a href="#" class="nav-link dropdown-toggle <?= in_array($currentPage, ['team.php', 'testimonial.php']) ? 'active' : '' ?>" data-bs-toggle="dropdown">Pages</a>
+                <div class="dropdown-menu fade-down m-0">
+                    <a href="team.php" class="dropdown-item <?= $currentPage == 'team.php' ? 'active' : '' ?>">Our Team</a>
+                    <a href="testimonial.php" class="dropdown-item <?= $currentPage == 'testimonial.php' ? 'active' : '' ?>">Testimonial</a>
                 </div>
-                <a href="faq.php" class="nav-item nav-link">FAQ</a>
-                <a href="contact.php" class="nav-item nav-link">Contact</a>
             </div>
-            <a href="signup.php" class="btn btn-primary py-4 px-lg-5 d-none d-lg-block">Join Now<i class="fa fa-arrow-right ms-3"></i></a>
+
+            <a href="faq.php" class="nav-item nav-link <?= $currentPage == 'faq.php' ? 'active' : '' ?>">FAQ</a>
+            <a href="contact.php" class="nav-item nav-link <?= $currentPage == 'contact.php' ? 'active' : '' ?>">Contact</a>
         </div>
-    </nav>
-    <!-- Navbar End -->
+
+        <a href="signup.php" class="btn btn-primary py-4 px-lg-5 d-none d-lg-block <?= $currentPage == 'signup.php' ? 'active' : '' ?>">Join Now<i class="fa fa-arrow-right ms-3"></i></a>
+    </div>
+</nav>
+<!-- Navbar End -->
+
+    
